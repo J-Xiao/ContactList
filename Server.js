72008bbaae895d7e5efba8094e3f197a83aa7280
J-Xiao <xiaojijundavid@gmail.com>
2016-08-16 20:23:36 -0400

    Add get function in node server

--- Server.js
+++ Server.js
@@ -3,5 +3,9 @@ var app = express();
 
 app.use(express.static(__dirname + "/ContactList"))
 
+app.get('contactlist', function(req, res){
+  console.log("GET request received!");
+})
+
 app.listen(3000);
 console.log('Server running on port 3000')
