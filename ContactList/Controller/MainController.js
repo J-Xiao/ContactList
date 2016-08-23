d067fb0248f1ee9a5e7a61ac74ab70bf85be499e
J-Xiao <xiaojijundavid@gmail.com>
2016-08-22 22:45:53 -0400

    Name the GET function as refresh to be frequently used

--- ContactList/Controller/MainController.js
+++ ContactList/Controller/MainController.js
@@ -3,12 +3,14 @@ var app = angular.module('myApp', []);
 app.controller('MainController',['$scope',function($scope){
   console.log("Hello World From AngularJS");
 
-  $http.get('/contactlist').success(function (res){
-    console.log("I got requested data!");
+  var refresh = function () {
+    $http.get('/contactlist').success(function (res){
+      console.log("I got requested data!");
 
-    $scope.contactlist = res;
-    $scope.contact = "";
-  })
+      $scope.contactlist = res;
+      $scope.contact = "";
+    });
+  };
 
   $scope.addContact = function () {
     console.log($scope.contact);
